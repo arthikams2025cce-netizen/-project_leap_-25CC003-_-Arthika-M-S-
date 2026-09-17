747ab83f5711b08ce2f8be0d6d78a4bf36794324
arthikams2025cce-netizen <arthika.ms2025cce@sece.ac.in>
2026-09-17 15:25:25 +0530

    Add files via upload

---
+++ ProjectLeap25cc003ArthikaMSApplication.java
@@ -0,0 +1,13 @@
+package com.example.project_leap_25cc003_arthika_m_s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectLeap25cc003ArthikaMSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectLeap25cc003ArthikaMSApplication.class, args);
+	}
+
+}
